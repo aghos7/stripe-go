656d5631bf3e4e22f432db231b8dda50673e44af
Remi Jannel <remi@stripe.com>
2019-05-03 22:34:43 -0700

    Add support for Customer Balance Transaction resource and APIs

---
+++ customerbalancetransaction/client_test.go
@@ -0,0 +1,37 @@
+package customerbalancetransaction
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	stripe "github.com/stripe/stripe-go"
+	_ "github.com/stripe/stripe-go/testing"
+)
+
+func TestCustomerBalanceTransactionGet(t *testing.T) {
+	transaction, err := Get("cbtxn_123", &stripe.CustomerBalanceTransactionParams{
+		Customer: stripe.String("cus_123"),
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, transaction)
+}
+
+func TestCustomerBalanceTransactionList(t *testing.T) {
+	i := List(&stripe.CustomerBalanceTransactionListParams{
+		Customer: stripe.String("cus_123"),
+	})
+
+	// Verify that we can get at least one transaction
+	assert.True(t, i.Next())
+	assert.Nil(t, i.Err())
+	assert.NotNil(t, i.CustomerBalanceTransaction())
+}
+
+func TestCustomerBalanceTransactionNew(t *testing.T) {
+	transaction, err := New(&stripe.CustomerBalanceTransactionParams{
+		Amount:   stripe.Int64(1234),
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, transaction)
+}
